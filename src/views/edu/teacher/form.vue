bfcdfcdde03cf981cfd8b818f32e0ba77ceb61a6
fzmsyx <sophiasaww@gmail.com>
2020-04-21 09:36:41 +0900

    前端

---
+++ src/views/edu/teacher/form.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    讲师表单
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/edu/teacher/list.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="app-container">
+    讲师列表
+  </div>
+</template>
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
